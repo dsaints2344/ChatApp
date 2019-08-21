df7cc04e8d2dcab9c76e39ec343fda92259b512b
Carlos Acosta <carlos9822@hotmail.com>
2019-08-20 20:47:20 -0400

    Add commands

---
+++ SocketProxy/Commands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketProxy
+{
+    public class Commands
+    {
+        public string connect { get =>nameof(connect); }
+        public string user_list { get =>nameof(user_list); }
+        public string send { get =>nameof(send); }
+        public string send_all { get =>nameof(send_all); }
+        public string message { get =>nameof(message); }
+        public string confirmed { get =>nameof(confirmed); }
+        public string exit { get =>nameof(exit); }
+        public string push_user_list { get =>nameof(push_user_list); }
+    }
+}
