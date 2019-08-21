c9dfadd3d0382973da7a5f05b844f5a1484d9e2c
Carlos Acosta <carlos9822@hotmail.com>
2019-08-20 20:48:50 -0400

    commit

--- SocketProxy/Commands.cs
+++ SocketProxy/Commands.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace SocketProxy
 {
-    public class Commands
+    public static class Commands
     {
-        public string connect { get =>nameof(connect); }
-        public string user_list { get =>nameof(user_list); }
-        public string send { get =>nameof(send); }
-        public string send_all { get =>nameof(send_all); }
-        public string message { get =>nameof(message); }
-        public string confirmed { get =>nameof(confirmed); }
-        public string exit { get =>nameof(exit); }
-        public string push_user_list { get =>nameof(push_user_list); }
+        public static string connect { get =>nameof(connect); }
+        public static string user_list { get =>nameof(user_list); }
+        public static string send { get =>nameof(send); }
+        public static string send_all { get =>nameof(send_all); }
+        public static string message { get =>nameof(message); }
+        public static string confirmed { get =>nameof(confirmed); }
+        public static string exit { get =>nameof(exit); }
+        public static string push_user_list { get =>nameof(push_user_list); }
     }
 }
